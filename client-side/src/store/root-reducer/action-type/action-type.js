7ab90a797ad9e27328a6e040c620fe36800c7b47
Raihan Robbani <raihan.robbani@mcpayment.co.id>
2022-07-21 23:16:41 +0700

    add actiont type store

---
+++ client-side/src/store/root-reducer/action-type/action-type.js
@@ -0,0 +1,5 @@
+export const ACTION_LOADING = 'user/action_loading';
+export const ACTION_ERROR = 'user/action_loading';
+export const ACTION_CREATE_KANBAN = 'user/create_kanban';
+export const ACTION_DELETE_TASK = 'user/delete_task';
+export const ACTION_UPDATE_STAGE_TASK = 'user/update_stage_task';
